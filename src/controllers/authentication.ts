f73985c8fadfa3afed49c3eb9ec24b46ec7c2660
Saanand Sthavara <saanand@protonmail.com>
2023-08-27 00:13:43 +0530

    added routes and controllers-auth

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -1,7 +1,24 @@
 import express from 'express';
-
+import { getUserByEmail, creteUser } from 'db/users';
+import { authentication, random } from 'helpers';
 export const register = async (req: express.Request, res: express.Response) => {
   try {
+    const { email, password, username } = req.body;
+    if (!email || !password || !username) {
+      return res.status(400);
+    }
+    const existingUser = await getUserByEmail(email);
+
+    if (existingUser) {
+      return res.sendStatus(400);
+    }
+    const salt = random();
+    const user = await creteUser({
+      email,
+      username,
+      authentication: { salt, password: authentication(salt, password) },
+    });
+    return res.status(200).json(user).end();
   } catch (error) {
     console.log('err', error);
     return res.sendStatus(400);
